593953a6c8b1ed300690f93a3f6840c1ca77138f
riddhi-regur <riddhi.jani@regur.net>
2026-04-16 18:25:47 +0530

    the login code runs fine

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -37,38 +37,39 @@ class RegisteredUserController extends Controller
         DB::beginTransaction();
 
         try {
-             $request->validate([
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
+            $request->validate([
+                'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+                'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            ]);
 
             $user = new User;
-            $user->email =  $request->email;
+            $user->email = $request->email;
             $user->password = Hash::make($request->password);
             $user->role_id = Role::ADMIN;
             $user->save();
-            
-             if (! $user->exists) {
+
+            if (! $user->exists) {
                 throw new Exception('User was not persisted.');
             }
 
-             $admin = new Admin;
+            $admin = new Admin;
             $admin->name = $request->name;
             $admin->user_id = $user->id;
-             $admin->save();
-             
-        event(new Registered($user));
+            $admin->save();
 
-        Auth::login($user);
-DB::commit();
-        return redirect(route('dashboard', absolute: false));
-        }catch (Exception $e) {
+            event(new Registered($user));
+
+            Auth::login($user);
+            DB::commit();
+
+            return redirect(route('dashboard', absolute: false));
+        } catch (Exception $e) {
             DB::rollBack();
-           
-              return redirect()->back()
-            ->with('error', $e->getMessage())
-            ->withInput();
+
+            return redirect()->back()
+                ->with('error', $e->getMessage())
+                ->withInput();
         }
-       
+
     }
 }
